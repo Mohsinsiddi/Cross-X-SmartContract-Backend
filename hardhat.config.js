2747fd876d0e52d18331bea483280e64eefe1e4f
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gail.com>
2022-05-28 16:45:37 +0530

    deployed smart contracts on ETH and BSC Testnets

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require("@nomiclabs/hardhat-ethers");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,13 +22,31 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+ module.exports = {
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 2000,
+      },
+    },
+  },
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    rinkbey: {
+      url: process.env.RINKBEY_HTTP_INFURA || "",
+      accounts: {
+        mnemonic: process.env.MNEMONICS,
+      },
+      chainId: 4,
+    },
+    bscTestnet: {
+      //url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      url:process.env.BINANCE_HTTP_INFURA,
+      accounts: {
+        mnemonic: process.env.MNEMONICS,
+      },
+      chainId: 97,
     },
   },
   gasReporter: {
@@ -36,5 +55,8 @@ module.exports = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 1000000,
   },
 };

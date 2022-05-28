2c786dcb7d2c9ca492d409ebf529efc0619b1a5b
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gail.com>
2022-05-28 11:00:06 +0530

    updated deploy scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const BridgeWrapper = await hre.ethers.getContractFactory("BridgeWrapper");
+  const bridgeWrapper = await BridgeWrapper.deploy();
 
-  await greeter.deployed();
+  await bridgeWrapper.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("BridgeWrapper Smart Contract deployed to:", bridgeWrapper.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere

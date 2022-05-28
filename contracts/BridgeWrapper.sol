aeaccb2153a84a75021e78dc0e04bff131fec6d6
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gail.com>
2022-05-28 10:58:10 +0530

    did smart contract setup for bridge

---
+++ contracts/BridgeWrapper.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract BridgeWrapper is Ownable {
+    constructor() {
+    }
+}
